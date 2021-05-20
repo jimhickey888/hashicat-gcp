cb9885dfb70c5fe0612b432583f6e8cfe2515d48
James Hickey <jimhickey@gmail.com>
2021-05-20 16:33:31 +0000

    Addd vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,13 @@
-network_name = "gaurav-network"
-project_id = var.project
-subnets = [
-  {
-    subnet_name   = "gaurav-subnet"
-    subnet_ip     = "10.100.10.0/24"
-    subnet_region = var.region
-  }
-]
+module "network" {
+  source  = "app.terraform.io/JamesHickey-training/network/google"
+  version = "2.5.0"
+  network_name = "gaurav-network"
+  project_id = var.project
+  subnets = [
+    {
+      subnet_name   = "gaurav-subnet"
+      subnet_ip     = "10.100.10.0/24"
+      subnet_region = var.region
+    }
+  ]
+}
